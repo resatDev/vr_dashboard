a12b87d733f3401ca47454237bfb4f74a84c9d6a
Reşat Memiş Unofficial <resatmemisunofficial@bc8.wls.metu.edu.tr>
2020-01-04 23:32:21 +0300

    module-card :: changing the style of module cards

--- src/components/card.js
+++ src/components/card.js
@@ -4,24 +4,11 @@ import ModuleImg from './../image/module.png';
 export default class Card extends Component {
     render() {
         return (
-            <div className="outer-card">
-                <div className="row">
-                    <div className="col s1 m7">
-                        <div className="card">
-                            <div className="card-image">
-                                <img src={ ModuleImg } alt='Module Card' />
-                                <span className="card-title">Card Title</span>
-                            </div>
-                            <div className="card-content">
-                                <p>I am a very simple card. I am good at containing small bits of information.
-                                I am convenient because I require little markup to use effectively.</p>
-                            </div>
-                            <div className="card-action">
-                                <a href="#">This is a Link!</a>
-                            </div>
-                        </div>
-                    </div>
+            <div className="outerCard">
+                <div className="upperCard">
+                     
                 </div>
+                <div className="lowerCard"></div>
             </div>
         )
     }
